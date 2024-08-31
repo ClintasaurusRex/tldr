195475bec85138346b6945b28cbc5b0c6706f040
Clint Hiles <clinthiles1992@gmail.com>
2024-08-31 12:55:20 -0700

    put one of the buttons in the right home

--- src/components/PopupItems.jsx
+++ src/components/PopupItems.jsx
@@ -1,4 +1,4 @@
-
+import Summarizer from "./Api";
 const PopupItems = function () {
 
   return (
@@ -12,9 +12,10 @@ const PopupItems = function () {
           <p id="summary-text">The other difference between them is that objects have property names so those have to be used in the destructuring part. Since array values are numerically ordered and without names, the order that we destructure is tied to what value we get -- in other words, first is the first variable in the destructure so it gets the first value of the array..</p>
         </section>
         <section class="buttons">
-          <button id="rewrite-btn">Re-Write</button>
+          <Summarizer />
+          {/* <button id="rewrite-btn">Re-Write</button>
           <button id="summarize-btn" >Summary</button>
-          <button id="copy-btn">Copy</button>
+          <button id="copy-btn">Copy</button> */}
         </section>
         <section class="text-area">
           <textarea id="text-input" placeholder="Text Area" aria-label="Text Area"></textarea>
