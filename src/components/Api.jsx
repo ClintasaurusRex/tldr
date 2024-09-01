59241bc509f4349beab2ec239ef6ff6a7ea7bd5e
Clint Hiles <clinthiles1992@gmail.com>
2024-08-31 20:05:31 -0700

    made helper function file and import/export works, made a small change to manifest.json to fix error code

--- src/components/Api.jsx
+++ src/components/Api.jsx
@@ -47,21 +47,6 @@ const Summarizer = (props) => {
     }
   };
 
-  // function handleRewrite() {
-  //   setRewrite(true);
-  //   const text = document.body.innerText;
-  //   summarizeContent(summary, true);
-  // }
-  // const handleSummarize = () => {
-  //   const text = document.body.innerText; // Extract text from the page
-  //   summarizeContent(text);
-  // };
-
-  // const handleCopy = () => {
-  //   navigator.clipboard.writeText(summary);
-  //   setCopyMessage('Copied to clipboard!');
-  // };
-
   return (
 
 
