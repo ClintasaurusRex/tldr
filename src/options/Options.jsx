093822c39e70b1b2db4d315e3e18ebc203ea0598
Clint Hiles <clinthiles1992@gmail.com>
2024-09-06 14:23:13 -0700

    dark mode almost there

--- src/options/Options.jsx
+++ src/options/Options.jsx
@@ -1,32 +1,12 @@
+import useDarkMode from '../helpers/useDarkMode';
 import './Options.scss';
-import { useState, useEffect } from 'react';
 
 const Options = () => {
-  const [darkMode, setDarkMode] = useState(false);
 
-  useEffect(() => {
-    chrome.storage.sync.get(['darkMode'], (result) => {
-      if (result.darkMode !== undefined) {
-        setDarkMode(result.darkMode);
-        applyDarkMode(result.darkMode);
-      }
-    });
-  }, []);
-
-  const darkModeChange = (event) => {
-    const isChecked = event.target.checked;
-    setDarkMode(isChecked);
-    chrome.storage.sync.set({ darkMode: isChecked });
-    applyDarkMode(isChecked);
-  };
-
-  const applyDarkMode = (isDarkMode) => {
-    if (isDarkMode) {
-      document.body.classList.add('dark-mode');
-    } else {
-      document.body.classList.remove('dark-mode');
-    }
-  };
+  const {
+    darkMode,
+    darkModeChange,
+  } = useDarkMode();
 
   return (
     <div className="options-container">
--- src/popup/Popup.jsx
+++ src/popup/Popup.jsx
@@ -2,23 +2,8 @@ import React, { useState, useEffect } from 'react';
 import './Popup.scss';
 import PopupItems from '../components/PopupItems';
 
+
 const Popup = () => {
-  const [isDarkMode, setIsDarkMode] = useState(false);
-
-  useEffect(() => {
-    // Retrieve the dark mode setting from chrome.storage
-    chrome.storage.sync.get(['darkMode'], (result) => {
-      setIsDarkMode(result.darkMode || false);
-    });
-  }, []);
-
-  useEffect(() => {
-    if (isDarkMode) {
-      document.body.classList.add('dark-mode');
-    } else {
-      document.body.classList.remove('dark-mode');
-    }
-  }, [isDarkMode]);
 
   return (
     <div className="Popup">
