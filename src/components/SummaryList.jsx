d7f4a1b08bd1e8fa3a6e48bb5cc425b5783acf23
NelsonCGSantos <nelsoncgsantos@gmail.com>
2024-09-09 14:20:43 -0600

    changed copied to only affect the select summary

--- src/components/SummaryList.jsx
+++ src/components/SummaryList.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import useSavedSummaries from '../helpers/useSavedSummaries.js';
 import useSummarizer from '../helpers/useSummarizer.js';
 import { copyToClipboard } from '../helpers/colinho';
@@ -10,12 +10,15 @@ const SummaryList = () => {
     handleDelete,
     summaries,
     downloadSummary
-  } = useSavedSummaries(); // Import the new downloadSummary function
+  } = useSavedSummaries();
 
-  const {
-    copyMessage,
-    setCopyMessage
-  } = useSummarizer();
+  // Track which summary was copied
+  const [copiedSummaryId, setCopiedSummaryId] = useState(null);
+
+  const handleCopy = (summary, id) => {
+    copyToClipboard(summary, () => setCopiedSummaryId(id)); 
+    setTimeout(() => setCopiedSummaryId(null), 3000); 
+  };
 
   return (
     <div className="summary-list">
@@ -26,21 +29,21 @@ const SummaryList = () => {
         <ul>
           {Object.values(summaries).map(({ url, summary, id }) => (
             <li key={url}>
-              <h3 id='summary-url' >{url}</h3>
-              <p id='summary-value' >{summary}</p>
-              <div className="saved-summary-btns" >
-                <button
-                  id="summary-buttons" onClick={() => handleDelete(id)}>Delete</button>
-                <button
-                  id="summary-buttons" onClick={() => copyToClipboard(summary, setCopyMessage)}>
+              <h3 id='summary-url'>{url}</h3>
+              <p id='summary-value'>{summary}</p>
+              <div className="saved-summary-btns">
+                <button id="summary-buttons" onClick={() => handleDelete(id)}>
+                  Delete
+                </button>
+                <button id="summary-buttons" onClick={() => handleCopy(summary, id)}>
                   Copy to Clipboard
                 </button>
-                <button
-                  id="summary-buttons" onClick={() => downloadSummary(url, summary)}>
+                <button id="summary-buttons" onClick={() => downloadSummary(url, summary)}>
                   Download
                 </button>
               </div>
-              {copyMessage && <div className="copy-message">{copyMessage}</div>}
+           
+              {copiedSummaryId === id && <div className="copy-message">Copied!</div>}
             </li>
           ))}
         </ul>
