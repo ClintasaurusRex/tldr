6415ab4cc193e67f7930026e24d4d28fcb321e58
NelsonCGSantos <nelsoncgsantos@gmail.com>
2024-10-07 12:34:11 -0600

    Fixed the response text to match the font size changes

--- src/components/ResponseApi.jsx
+++ src/components/ResponseApi.jsx
@@ -1,11 +1,14 @@
+import React from "react";
+import useFontSize from "../helpers/useFontSize"; 
 
 const ResponseApi = function (props) {
   const { responseText } = props;
+  const { fontSize } = useFontSize(); 
 
   return (
     <div className="response-text">
-      <h2>Response</h2>
-      <p>{responseText}</p>
+      <h2 style={{ fontSize: fontSize }}>Response</h2> 
+      <p style={{ fontSize: fontSize }}>{responseText}</p> 
     </div>
   );
 };
