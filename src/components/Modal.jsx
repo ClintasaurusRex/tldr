928a00daa97843a8a1d656392f6672b1afaa63d4
Clint <clinthiles1992@gmail.com>
2024-10-10 17:10:40 -0700

    Put a btn in for the donate page in the modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,16 +1,25 @@
 import React from "react";
+import { openDonationsPage } from "../helpers/usePages";
 import "./Modal.scss";
 
 const Modal = ({ isOpen, onClose, children }) => {
   if (!isOpen) return null;
+  // const { openDonationsPage } = usePages();
 
   return (
     <div className="modal-overlay">
       <div className="modal-content">
         {children}
-        <button onClick={onClose} className="modal-close-btn">
-          Close
-        </button>
+        <div className="modal-btns">
+          <button onClick={onClose} className="modal-close-btn">
+            Close
+          </button>
+          <button>
+            <a href="#" onClick={openDonationsPage}>
+              Donate
+            </a>
+          </button>
+        </div>
       </div>
     </div>
   );
