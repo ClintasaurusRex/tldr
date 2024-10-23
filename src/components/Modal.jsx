95a5d5e895eed6b29352a4c0e219c0e01d48f943
NelsonCGSantos <nelsoncgsantos@gmail.com>
2024-10-23 16:34:53 -0600

    modal changes pt2

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,20 +1,31 @@
 import React from "react";
 import usePages from "../helpers/usePages";
+import useSound from "../helpers/useSound";
+import useFontSize from "../helpers/useFontSize"; 
 import "./Modal.scss";
 
 const Modal = ({ isOpen, onClose, children }) => {
   if (!isOpen) return null;
   const { openDonationsPage } = usePages();
+  const { playSound } = useSound(0.2); 
+  const { fontSize } = useFontSize(); 
 
   return (
     <div className="modal-overlay">
-      <div className="modal-content">
+      <div className="modal-content" style={{ fontSize: fontSize }}>
         {children}
         <div className="modal-btns">
-          <button onClick={onClose} className="modal-close-btn">
+          <button
+            onClick={() => {
+              playSound();
+              onClose();
+            }}
+            className="modal-close-btn"
+            style={{ fontSize: fontSize }} 
+          >
             Close
           </button>
-          <button>
+          <button style={{ fontSize: fontSize }}>
             <a href="#" onClick={openDonationsPage}>
               Donate
             </a>
