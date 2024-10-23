dad31759ceb329bf30d17795a05b423abec42be0
Clint <clinthiles1992@gmail.com>
2024-10-23 15:26:40 -0700

    Refactored out comments in SummarizeBtns.jsx

--- src/components/SummarizeBtns.jsx
+++ src/components/SummarizeBtns.jsx
@@ -14,42 +14,6 @@ const SummarizeBtns = function (props) {
     handleSummarizeEntirePageWithChrome
   );
 
-  // const { playSound } = useSound(0.2);
-
-  // // State to track the number of clicks for each button
-  // const [selectionClickCount, setSelectionClickCount] = useState(0);
-  // const [pageClickCount, setPageClickCount] = useState(0);
-
-  // const [isModalOpen, setIsModalOpen] = useState(false);
-
-  // const handleSelectionClick = () => {
-  //   const newCount = selectionClickCount + 1;
-  //   setSelectionClickCount(newCount);
-
-  //   if (newCount % 2 === 0) {
-  //     setIsModalOpen(true); // Show the modal
-  //     setSelectionClickCount(0); // Reset the count after every two clicks
-  //     return; // Exit the function to prevent summarization
-  //   }
-
-  //   handleSummarizeSelection();
-  //   playSound();
-  // };
-
-  // const handlePageClick = () => {
-  //   const newCount = pageClickCount + 1;
-  //   setPageClickCount(newCount);
-
-  //   if (newCount % 2 === 0) {
-  //     setIsModalOpen(true); // Show the modal
-  //     setPageClickCount(0); // Reset the count after every two clicks
-  //     return; // Exit the function to prevent summarization
-  //   }
-
-  //   handleSummarizeEntirePageWithChrome();
-  //   playSound();
-  // };
-
   return (
     <div className="summarize-btns">
       {/* Button to Summarize Highlighted Text */}
